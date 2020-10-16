2cd09554ea169b6d4ea87ee92de7ab5f39082b44
hayinanjay <72958200+hayinanjay@users.noreply.github.com>
2020-10-16 14:29:38 +0700

    Add files via upload

---
+++ src/pbo1/pkg10119005/latihan13/PBO110119005Latihan13.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo1.pkg10119005.latihan13;
+
+/**
+ *
+ * @author Hayin
+ * NAMA     : Hayin Ananta
+ * KELAS    : IF-1
+ * NIM      : 10119005
+ * Deskripsi Program : Relational
+ */
+public class PBO110119005Latihan13 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int bill = 10;
+        int bill2 = 20;
+        System.out.println("a == b == " + (bill ==  bill2 ));
+        System.out.println("a != b = " + (bill != bill2));
+        System.out.println("a > b = " + (bill > bill2));
+        System.out.println("a < b = " + (bill < bill2));
+        System.out.println("b >= a = " + (bill2 >= bill));
+        System.out.println("b <= a = " + (bill2 <= bill));
+    }
+    
+}
